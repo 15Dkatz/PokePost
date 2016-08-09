50d78f3ff0ed37778b811da9caf3482c9b90fc09
15Dkatz <15Dkatz@shcp.edu>
2016-08-09 08:44:43 -0700

    adding to ref detail

--- src/components/app-ref-detail.js
+++ src/components/app-ref-detail.js
@@ -5,6 +5,22 @@ import {
   TextInput
 } from 'react-native';
 
+import styles from '../styles';
+import {ref} from './auth/authentication';
+
 module.exports = React.createClass({
+	getInitialState() {
+		return ({
+			dataSource: ds.cloneWithRows(''),
+			text: '',
+			deatilRef ''
+		})
+	}
+
+	componentDidMount() {
+		// this passed props is the uid of the ref that 
+		const detailRef = ref.child(this.props.ref_uid);
+		this.setState({detailRef})
+	}
   
 })
